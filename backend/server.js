a34aaee48a98f5efbc2d02ad982444759d4cbe7e
Aileen7Rose <aileenalbin@gmail.com>
2026-04-11 02:36:08 +0530

    updated frontend url in server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-  origin: 'https://your-frontend.vercel.app',
+  origin: 'https://nitc-healthcare-center-murex.vercel.app',
   credentials: true
 }));
 // Database connection
